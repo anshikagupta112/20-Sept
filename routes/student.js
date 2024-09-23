1d4b76fb4099b32b4c87d2d3ffa29bbd3a4a0eb5
Garima Gupta <garima@atlassian.com>
2024-09-23 15:27:23 +0530

    Code committed

--- routes/student.js
+++ routes/student.js
@@ -8,4 +8,8 @@ router.get('/',(req,res)=>{
 router.post('/add/student',(req,res)=>{
     studentcontroller.addStudent(req,res)
 })
+    router.get('/students',(req,res)=>{
+        studentcontroller.getStudents(req,res)
+    })
+
 module.exports=router
